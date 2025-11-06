eba785ada7cdfc2370d8f3e257b13b1b6357e8c8
Daishik <crucisconsulting@outlook.com>
2025-11-06 12:15:58 +0000

    Broken unit test fixed - missing store provider

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -3,6 +3,7 @@ import { App } from "./app";
 import { provideZonelessChangeDetection } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { WindowsService } from "./services/windows.service";
+import { provideMockStore } from "@ngrx/store/testing";
 
 describe('AppComponent', () => {
   let fixture: ComponentFixture<App>;
@@ -14,6 +15,7 @@ describe('AppComponent', () => {
         provideZonelessChangeDetection(),
         { provide: ActivatedRoute, useValue: { snapshot: { data: {} } } },
         WindowsService,
+        provideMockStore(),
         { provide: ComponentFixtureAutoDetect, useValue: true },
       ],
       imports: [App]
